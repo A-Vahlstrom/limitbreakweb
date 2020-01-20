6ce8524e68288975d6e1c5927c6195aa68c8f991
JHenrysson <56089745+JHenrysson@users.noreply.github.com>
2020-01-20 20:06:59 +0100

    adding header to all pages using js
    created js file that will allow us to edit header using one html file updating all the pages if a change is made to the header

---
+++ main.js
@@ -0,0 +1,12 @@
+window.addEventListener('load', function () { // built in function called - w/two parameters
+    let rawFile = new XMLHttpRequest(); //declared variable = a new request
+    rawFile.onreadystatechange = () => { // calling built in function on variable(rawFile)
+        if (rawFile.readyState === 4 && (rawFile.status === 200 || rawFile.status === 0)) {// checks "response" http status code
+            document.getElementById('header').insertAdjacentHTML('afterbegin', rawFile.responseText);
+        }// calls dom / places header.html in the header div // afterbegin parameter of insertadjacentHTML / precreated variable- containing header code
+    };
+
+    rawFile.open('GET', 'header.html', true); // opening header.html
+    rawFile.setRequestHeader('Content-type', 'text/html');//tells the type of content = html (this case)
+    rawFile.send();// performs the request
+});
